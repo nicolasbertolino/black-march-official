bd7594ee078b13349ffd27e220dcee1f07f10ec8
Nicolas Bertolino <bertolinonicolas@gmail.com>
2021-02-13 15:56:28 +0100

    Get ready to rumble

--- js/main.js
+++ js/main.js
@@ -32,22 +32,75 @@ gsap.from(".socials li", {
   ease: "power4",
 });
 
-gsap.from("h1 span", {
+gsap.from("h1", {
   opacity: 0,
   y: 20,
   skewY: 1,
   duration: 1.5,
   delay: 4.4,
-  stagger: 0.1,
   ease: "power4",
 });
 
-gsap.from(".intro figure", {
+gsap.from(".intro", {
   opacity: 0,
   y: 20,
-  skewY: 5,
-  rotate: 1,
   duration: 1,
   delay: 4.6,
+  ease: "power4",
+});
+
+gsap.from(".review", {
+  scrollTrigger: {
+    trigger: ".review",
+    start: "top 70%"
+  },
+  opacity: 0,
+  y: 20,
+  duration: 1.5,
+  ease: "power4",
+});
+
+gsap.from(".music article", {
+  scrollTrigger: {
+    trigger: ".music",
+    start: "top 70%"
+  },
+  opacity: 0,
+  y: 20,
+  duration: 1.5,
+  stagger: 0.2,
+  ease: "power4",
+});
+
+gsap.from(".live", {
+  scrollTrigger: {
+    trigger: ".live",
+    start: "top 70%"
+  },
+  opacity: 0,
+  y: 20,
+  duration: 1.5,
+  ease: "power4",
+});
+
+gsap.from(".contact", {
+  scrollTrigger: {
+    trigger: ".contact",
+    start: "top 70%"
+  },
+  opacity: 0,
+  y: 20,
+  duration: 1.5,
+  ease: "power4",
+});
+
+gsap.from("footer", {
+  scrollTrigger: {
+    trigger: "footer",
+    start: "top 70%"
+  },
+  opacity: 0,
+  y: 20,
+  duration: 1.5,
   ease: "power4",
 });
